4b3c3ed9039b4f415c32ed645c0621948830020d
Daniele Covallero <daniele.covallero@akqua.com>
2021-12-19 20:09:18 +0100

    shorten tests code

--- src/SentenceParser.test.js
+++ src/SentenceParser.test.js
@@ -3,37 +3,11 @@ import test from 'ava';
 import * as expected from '../mock/expected/index.js';
 import SentenceParser from '../src/SentenceParser.js';
 
-test('Step 1', (t) => {
-  const step = new SentenceParser(1).parse();
-  t.deepEqual(step, expected.step_1);
-});
+function stepTest(num) {
+  test(`Step ${num}`, (t) => {
+    const step = new SentenceParser(num).parse();
+    t.deepEqual(step, expected[`step_${num}`]);
+  });
+}
 
-test('Step 2', (t) => {
-  const step = new SentenceParser(2).parse();
-  t.deepEqual(step, expected.step_2);
-});
-
-test('Step 3', (t) => {
-  const step = new SentenceParser(3).parse();
-  t.deepEqual(step, expected.step_3);
-});
-
-test('Step 4', (t) => {
-  const step = new SentenceParser(4).parse();
-  t.deepEqual(step, expected.step_4);
-});
-
-test('Step 5', (t) => {
-  const step = new SentenceParser(5).parse();
-  t.deepEqual(step, expected.step_5);
-});
-
-test('Step 6', (t) => {
-  const step = new SentenceParser(6).parse();
-  t.deepEqual(step, expected.step_6);
-});
-
-test('Step 7', (t) => {
-  const step = new SentenceParser(7).parse();
-  t.deepEqual(step, expected.step_7);
-});
+[1, 2, 3, 4, 5, 6, 7].forEach(stepTest);
